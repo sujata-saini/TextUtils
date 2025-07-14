e647cec83416fa9775445001f449209b9d10ec6d
sujata-saini <sainisujata303@gmail.com>
2025-07-14 18:58:21 +0530

    toggle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -50,6 +50,11 @@
 //   </nav>
 //   )
 // }
+import React from 'react';
+import { Link } from "react-router-dom";
+
+export default function Navbar(props) {
+  return (
 <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
   <div className="container-fluid">
     <Link className="navbar-brand" to="/">TextUtils</Link>
@@ -72,3 +77,5 @@
     </div>
   </div>
 </nav>
+  );
+}
